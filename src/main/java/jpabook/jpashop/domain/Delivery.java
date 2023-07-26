3cee8a2c3cd7e2ae9f5d1fd27af7aec8de6200bd
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2023-07-26 23:15:08 +0900

    feat: 엔터티 클래스 생성(회원, 주문, 배송, 상품)

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) // ORDINAL 은 중간에 값이 추가되면 오류가 발생할 수 있음
+    private DeliveryStatus status; // READY, COMP
+
+}
