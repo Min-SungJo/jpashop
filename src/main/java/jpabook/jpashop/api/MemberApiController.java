d6dae75d4b922a8e0881f0f73fb8502c2c83610b
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2023-08-02 14:37:15 +0900

    feat: 회원 조회 api

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,6 +17,33 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public  Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count; // 스펙 추가에 유연하게 대응할 수 있음
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto { // 엔티티를 DTO 로 변환해, API 스펙에 노출할 정보만 노출
+        private String name;
+    }
     /**
      * 등록 V1: 요청 값으로 Member 엔티티를 직접 받는다.
      * 문제점
