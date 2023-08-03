efddd5807521afd4b4d6f150de513bdbc4955d7e
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2023-08-03 18:28:05 +0900

    build: Hibernate5Module 추가

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -12,6 +14,13 @@ public class JpashopApplication {
 		String data = hello.getData();
 		System.out.println("data = " + data);
 		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		Hibernate5Module hibernate5Module = new Hibernate5Module();
+//		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true); // 엔티티를 외부로 노출하면 안된다
+		return hibernate5Module;
 	}
 
 }
